f4d051ba71e16042f9c339b130b2ee9698cfbbdd
gavGaz <gavGaz@users.noreply.github.com>
2017-05-24 21:01:45 +0100

    Update to fix switch to Azure Translator API
    Update to fix switch to Azure Translator API - old API deprecated

--- token.php
+++ token.php
@@ -1,18 +1,22 @@
 <?php
-$ClientID="scriptTranslate-01";
-$ClientSecret="4evraNcG2aHC0uBsUAr2TYybntciPEwWht9fGaegdMw=";
+$azure_key = "277fae4b182b47eb927040b074c6ff4d";
 
-$ClientSecret = urlencode ($ClientSecret);
-$ClientID = urlencode($ClientID);
+$azure_key = urlencode ($azure_key);
 
 // Get a 10-minute access token for Microsoft Translator API.
-$url = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13";
-$postParams = "grant_type=client_credentials&client_id=$ClientID&client_secret=$ClientSecret&scope=http://api.microsofttranslator.com";
+$url = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken";
 
 $ch = curl_init();
+$data_string = json_encode('{body}');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string),
+    'Ocp-Apim-Subscription-Key: ' . $azure_key
+));
 curl_setopt($ch, CURLOPT_URL, $url); 
+curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postParams);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);  
 $rsp = curl_exec($ch); 
 
